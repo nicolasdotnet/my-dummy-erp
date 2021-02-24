919a16b12aefaca2b29a023c1151fb71256fcc4b
nicolasdotnet <nicolas.desdevises@gmail.com>
2021-02-24 12:21:40 +0100

    jenkis auto build

---
+++ src/myerp-technical/src/main/java/com/dummy/myerp/technical/exception/Commit.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dummy.myerp.technical.exception;
+
+/**
+ *
+ * @author nicolasdotnet
+ */
+public class Commit {
+    
+}
